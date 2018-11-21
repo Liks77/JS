3c0f25d0b7c72e15523017eaa0ad1034b133831e
Liks77 <saleksandrov7goit@gmail.com>
2018-11-22 00:23:13 +0200

    module 08

--- module 07/index.js
+++ module 07/index.js
@@ -21,51 +21,43 @@ const posts = [
     }
   ];
 
-  function createPostCard(posts) {
-  for (let i of posts) {
+  function createPostCard(post) {
+//     <!-- <div class="post">
+//     <img class="post__image" src="http://via.placeholder.com/400x150" alt="post image">
+//     <h2 class="post__title">Lorem ipsum dolor</h2>
+//     <p class="post__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!</p>
+//     <a class="button" href="#">Read more</a>
+// </div> -->
+    let div = document.createElement('div');
+    div.classList.add('post');
 
-    // @param trackingElement
-
-    let body = document.querySelector('.body');
+    const image = document.createElement('img');
+    image.classList.add('post__image');
+    image.setAttribute('src', post.img);
+    image.setAttribute('alt', 'post image');
   
-    // @param createElement
-
-    let post = document.createElement('div');
-    let img = document.createElement('img');
-    let h2 = document.createElement('h2');
-    let p = document.createElement('p');
-    let a = document.createElement('a');
+    const postTitle = document.createElement('h2');
+    postTitle.classList.add('post__title');
+    postTitle.textContent = post.title;
   
-    //  @param addElement's class
+    const postText = document.createElement('p');
+    postText.classList.add('post__text');
+    postText.textContent = post.text;
 
-    post.classList.add('post');
-    img.classList.add('post__image');
-    h2.classList.add('post__title');
-    p.classList.add('post__text');
-    a.classList.add('button');
-
-    // @param addElement's attribute
+    const postLink = document.createElement('a');
+    postLink.classList.add('button');
+    postLink.textContent = post.link;
+    postLink.setAttribute('href', '#');
   
-    img.setAttribute('src', i.img);
-    img.setAttribute('alt', 'post image');
-    a.setAttribute('href', '#');
- 
-    // @param add text to Element
-
-    h2.textContent = i.title;
-    p.textContent =i.text;
-    a.textContent = i.link;
+    div.append(image);
+    div.append(postTitle);
+    div.append(postText);
+    div.append(postLink);
   
-    // @param put Element to DOMcontent
-
-    body.append(post)
-    post.append(img)
-    post.append(h2)
-    post.append(p)
-    post.append(a)
+    return div;
   }
-};
-
-  createPostCard (posts);
-  console.log(posts.length);
-
+  
+  
+  const root = document.querySelector('#root');
+  root.append(createPostCard(posts));
+  
